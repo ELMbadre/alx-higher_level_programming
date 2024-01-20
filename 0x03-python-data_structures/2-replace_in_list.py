fc470a45ff3cc8c7a323f34a692cccf3957d1fbb
ELMbadre <badreddineelmoutarif@gmail.com>
2024-01-20 21:40:59 +0100

    All tasks finished! 🏁

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 2-replace_in_list.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
